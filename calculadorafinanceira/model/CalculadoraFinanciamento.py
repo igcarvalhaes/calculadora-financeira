12222dee06c3fc35a0cd5c500a02ce170497af41
Matheus Martins <matheusmr.95@gmail.com>
2019-06-02 20:50:24 -0300

    Alterando os campos da linha da tabela

--- calculadorafinanceira/model/CalculadoraFinanciamento.py
+++ calculadorafinanceira/model/CalculadoraFinanciamento.py
@@ -4,9 +4,9 @@ class CalculadoraFinanciamentoSacPrice:
     
     def calcula_financiamento(self, valor_bem, taxa_juros, num_parcelas, sistema_financiamento):
         if (sistema_financiamento is SistemaFinanciamento.PRICE):
-            pass
+            calcula_financiamento_price(valor_bem, taxa_juros, num_parcelas, sistema_financiamento)
         elif (sistema_financiamento is SistemaFinanciamento.SAC):
-            pass
+            calcula_financiamento_sac(valor_bem, taxa_juros, num_parcelas, sistema_financiamento)
         
     def calcula_financiamento_price(self, valor_bem, taxa_juros, num_parcelas, sistema_financiamento):
         pass
--- calculadorafinanceira/model/LinhaCalculoFinanciamento.py
+++ calculadorafinanceira/model/LinhaCalculoFinanciamento.py
@@ -1,8 +1,6 @@
 class LinhaCalculoFinanciamentoSacPrice:
     
-    def __init__(self, parcela, prestacao, amortizacao, juros, saldo_devedor):
-        self.parcela = parcela
-        self.prestacao = prestacao
-        self.amortizacao = amortizacao
-        self.juros = juros
-        self.saldo_devedor = saldo_devedor
+    def __init__(self, linha, coluna, table_item):
+        self.linha = linha
+        self.coluna = coluna
+        self.table_item = table_item
